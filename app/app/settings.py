6ec1b37bc9c0fdffa30274db5b4c48969ea2c71c
Boyukkhan <beyukxan12@gmail.com>
2022-06-16 13:43:15 +0400

    new

--- app/app/settings.py
+++ app/app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
